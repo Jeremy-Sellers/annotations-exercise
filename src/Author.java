cc52d3894d5f34591d313ff6bbeb3c93a07c03fa
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-04-07 10:45:57 -0500

    complete easy fix

--- src/Author.java
+++ src/Author.java
@@ -4,6 +4,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
@@ -28,8 +29,8 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
-    public String sortName() {
+    @Override//Changed sortName to fullName to match person superClass
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
